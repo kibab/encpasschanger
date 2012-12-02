ba8ecf8a66c9bca63a2f9163d740f40fc9aa3d98
Ilya Bakulin <ilya@bakulin.de>
2012-12-02 16:00:22 +0100

    - Jelly Bean fixes; - Escape dangerous characters; - Progress report; - "Debug output" to ease debugging problems with encryption process;

---
+++ src/com/kibab/android/EncPassChanger/ChangePassProgress.java
@@ -0,0 +1,14 @@
+package com.kibab.android.EncPassChanger;
+
+public class ChangePassProgress {
+	
+	private String logMsg;
+	
+	public ChangePassProgress(String logMsg) {
+		this.logMsg = logMsg;
+	}
+	
+	public String getLogMsg() {
+		return logMsg;
+	}
+}
